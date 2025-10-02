f162591aa9b5e620f83ba5c6d017b80483d20a56
ArtemKukol <119360387+ArtemKukol@users.noreply.github.com>
2025-10-02 15:26:27 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,38 +1,49 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const counts = document.querySelectorAll('.count')
-	const totalCells = document.querySelectorAll('.total')
-	const totalSumElement = document.getElementById('total-sum')
-	const controlSumInput = document.getElementById('control-sum')
-	const deviationElement = document.getElementById('deviation')
+	const counts = document.querySelectorAll('.count');
+	const totalCells = document.querySelectorAll('.total');
+	const totalSumElement = document.getElementById('total-sum');
+	const controlSumInput = document.getElementById('control-sum');
+	const deviationElement = document.getElementById('deviation');
 
 	counts.forEach((count, index) => {
 		count.addEventListener('input', () => {
 			const nominal = parseFloat(
 				count.parentElement.previousElementSibling.querySelector('input').value
-			)
-			const quantity = parseFloat(count.value)
-			const total = nominal * quantity
+			);
+			const quantity = parseFloat(count.value);
+			const total = nominal * quantity;
 
-			totalCells[index].textContent = total.toFixed(2)
-			calculateTotalSum()
-		})
-	})
+			totalCells[index].textContent = formatNumber(total);
+			calculateTotalSum();
+		});
+	});
 
-	controlSumInput.addEventListener('input', calculateDeviation)
+	controlSumInput.addEventListener('input', calculateDeviation);
 
 	function calculateTotalSum() {
-		let totalSum = 0
+		let totalSum = 0;
 		totalCells.forEach(cell => {
-			totalSum += parseFloat(cell.textContent)
-		})
-		totalSumElement.textContent = totalSum.toFixed(2)
-		calculateDeviation()
+			totalSum +=
+				parseFloat(cell.textContent.replace(/\s/g, '').replace(',', '.')) || 0;
+		});
+		totalSumElement.textContent = formatNumber(totalSum);
+		calculateDeviation();
 	}
 
 	function calculateDeviation() {
-		const totalSum = parseFloat(totalSumElement.textContent)
-		const controlSum = parseFloat(controlSumInput.value) || 0
-		const deviation = totalSum - controlSum
-		deviationElement.textContent = deviation.toFixed(2)
+		const totalSum =
+			parseFloat(
+				totalSumElement.textContent.replace(/\s/g, '').replace(',', '.')
+			) || 0;
+		const controlSum = parseFloat(controlSumInput.value) || 0;
+		const deviation = totalSum - controlSum;
+		deviationElement.textContent = formatNumber(deviation);
 	}
-})
+
+	function formatNumber(num) {
+		return num.toLocaleString('ru-RU', {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 2,
+		});
+	}
+});
